5158c54d5e31c5ba28e56a901ccbaff389625a52
Arseniy Graur <argraur@aosip.dev>
2023-08-14 23:17:34 +0800

    [SQUASH] statix: Import signing target from hentaiOS
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: I904dc07d3e258595e8db9cb02b49353758ae0987

---
+++ build/tasks/stx_signed.mk
@@ -0,0 +1,70 @@
+#
+# Copyright (C) 2018-2019 The Pixel3ROM Project
+# Copyright (C) 2020 Raphielscape LLC. and Haruka LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+SIGNED_TARGET_FILES_PACKAGE := $(PRODUCT_OUT)/$(TARGET_DEVICE)-target_files-$(BUILD_ID_LC).zip
+SIGN_FROM_TARGET_FILES := $(HOST_OUT_EXECUTABLES)/sign_target_files_apks$(HOST_EXECUTABLE_SUFFIX)
+
+$(SIGNED_TARGET_FILES_PACKAGE): $(BUILT_TARGET_FILES_PACKAGE) \
+		$(INTERNAL_OTATOOLS_FILES) $(SIGN_FROM_TARGET_FILES)
+	@echo "Signed target files package: $@"
+	    $(SIGN_FROM_TARGET_FILES) --verbose \
+	    -o \
+	    -p $(OUT_DIR)/host/linux-x86 \
+	    -d $(PROD_CERTS) \
+	    $(BUILT_TARGET_FILES_PACKAGE) $@
+
+.PHONY: signed-target-files-package
+signed-target-files-package: $(SIGNED_TARGET_FILES_PACKAGE)
+
+PROD_OTA_PACKAGE_TARGET := $(PRODUCT_OUT)/$(STATIX_VERSION).zip
+
+$(PROD_OTA_PACKAGE_TARGET): KEY_CERT_PAIR := $(PROD_CERTS)/releasekey
+
+$(PROD_OTA_PACKAGE_TARGET): $(BRO)
+
+$(PROD_OTA_PACKAGE_TARGET): $(SIGNED_TARGET_FILES_PACKAGE) \
+		$(INTERNAL_OTATOOLS_FILES) $(OTA_FROM_TARGET_FILES)
+	@echo "statix production: $@"
+	    $(OTA_FROM_TARGET_FILES) --verbose \
+	    -p $(OUT_DIR)/host/linux-x86 \
+	    -k $(KEY_CERT_PAIR) \
+	    $(SIGNED_TARGET_FILES_PACKAGE) $@
+
+.PHONY: stx-prod
+stx-prod: $(PROD_OTA_PACKAGE_TARGET)
+
+ifneq ($(PREVIOUS_TARGET_FILES_PACKAGE),)
+
+INCREMENTAL_OTA_PACKAGE_TARGET := $(PRODUCT_OUT)/incremental-$(STATIX_VERSION).zip
+
+$(INCREMENTAL_OTA_PACKAGE_TARGET): KEY_CERT_PAIR := $(PROD_CERTS)/releasekey
+
+$(INCREMENTAL_OTA_PACKAGE_TARGET): $(BRO)
+
+$(INCREMENTAL_OTA_PACKAGE_TARGET): $(SIGNED_TARGET_FILES_PACKAGE) \
+		$(INTERNAL_OTATOOLS_FILES) $(OTA_FROM_TARGET_FILES)
+	@echo "statix incremental production: $@"
+	    $(OTA_FROM_TARGET_FILES) --verbose \
+	    -p $(OUT_DIR)/host/linux-x86 \
+	    -k $(KEY_CERT_PAIR) \
+	    -i $(PREVIOUS_TARGET_FILES_PACKAGE) \
+	    $(SIGNED_TARGET_FILES_PACKAGE) $@
+
+.PHONY: incremental-ota
+incremental-ota: $(INCREMENTAL_OTA_PACKAGE_TARGET)
+
+endif
