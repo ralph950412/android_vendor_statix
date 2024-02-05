d756391c35814e9e77c84621b392eb8cf2f158b1
Linux4 <tim@linux4.de>
2024-02-05 20:56:40 +0800

    statix: Make powershare node customizable
    Original commit:
    https://github.com/AOSPA/android_vendor_aospa/commit/0a28b04564f974f5b74a7b5044528eae6ed67428
    
    Co-authored-by: Anay Wadhera <anay@aospa.co>
    Co-authored-by: Jake Weinstein <jake@aospa.co>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: I92f3e2405792ce6d493499f90df262d8bf86f93a
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -3,6 +3,7 @@ SOONG_CONFIG_statixGlobalVars += \
     aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    powershare_node \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -49,6 +50,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 SOONG_CONFIG_statixGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_statixGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_statixGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
+SOONG_CONFIG_statixGlobalVars_powershare_node := $(TARGET_POWERSHARE_NODE)
 SOONG_CONFIG_statixGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_statixGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_statixGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
